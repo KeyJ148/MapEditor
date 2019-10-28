4059cb49afa791011aab0b00e1ba7a48af60965c
Евгений Серов <kejik148@gmail.com>
2019-10-28 10:01:56 +0300

    fix #3

--- src/logic/MapEditor.java
+++ src/logic/MapEditor.java
@@ -2,10 +2,15 @@ package logic;
 
 import gui.FrameMain;
 
+import java.io.File;
+
 public class MapEditor {
 
     public static void main(String[] args) {
         FrameMain.getInstance();
+
+        Storage.loadSprites(new File("./res/image/"));
+        FrameMain.getInstance().getPanelMain().getPanelTree().addSprite(Storage.getSprite());
     }
 
 }
